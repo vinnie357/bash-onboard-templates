b6796bc3e275dd74db66213bce72275e52a7dcb7
vinnie <vinnie.mazza@gmail.com>
2020-10-23 13:52:08 -0400

    f5 ext moved to devcentral!

--- terraform/azure/sca/onboard.sh
+++ terraform/azure/sca/onboard.sh
@@ -95,7 +95,7 @@ WantedBy=multi-user.target
 EOF
 sudo systemctl enable --now code-server
 # install extensions for coder as user
-extensionUrls="https://api.github.com/repos/DumpySquare/vscode-f5-fast/releases/latest https://api.github.com/repos/hashicorp/vscode-terraform/releases/latest"
+extensionUrls="https://api.github.com/f5devcentral/vscode-f5/releases/latest https://api.github.com/repos/hashicorp/vscode-terraform/releases/latest"
 for downloadUrl in $extensionUrls
 do
     wget $(curl -s $downloadUrl | jq -r '.assets[] | select(.name | contains (".vsix")) | .browser_download_url')
